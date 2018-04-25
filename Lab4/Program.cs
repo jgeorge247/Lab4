c8ce9d26cdbb6f92464dc078919739257f008ebb
jgeorge247 <justingeorge247@gmail.com>
2018-04-25 17:47:09 -0400

    first commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a Table of Powers");
+
+            string UserChoice = "";
+            do
+            {
+                Console.WriteLine("\nPlease enter an integer");
+                int Num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Number\tSquared\t Cubed");
+
+                for (int i = 1; i <= Num; i++)
+                {
+                    Console.WriteLine($"{i}\t{Math.Pow(i, 2)}\t {Math.Pow(i, 3)}");
+                }
+
+                do
+                {
+
+                    Console.Write("Would you like to try again? (Y/N) ");
+
+                    UserChoice = Console.ReadLine();
+                    if (UserChoice != "Y" && UserChoice != "N")
+                    {
+                        Console.WriteLine("Invalid Input. Please Write (Y/N)");
+                    }
+                } while (UserChoice != "Y" && UserChoice != "N");
+
+            } while (UserChoice == "Y");
+        } 
+    }
+
+}
